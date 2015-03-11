0a27cb45a6472c57cb58b31cf68eca7304d174fa
Mohana <kmravali@users.noreply.github.com>
2015-03-11 01:15:27 +0000

    Update DecimalToRoman.java

--- TDD_Examples/src/com/tdd/example/decimaltoroman/DecimalToRoman.java
+++ TDD_Examples/src/com/tdd/example/decimaltoroman/DecimalToRoman.java
@@ -1,34 +1,53 @@
 package com.tdd.example.decimaltoroman;
 
-public class DecimalToRoman {
+import static org.junit.Assert.*;
 
-	public Object getRomanNumber(int decimal) {
-		String result = "";
+import org.junit.Before;
+import org.junit.Test;
 
-		
-		if (decimal == 10)
-			result = "X";
-		if (decimal == 9)
-			result = "IX";
+public class DecimalToRomanTest {
+	
+	private DecimalToRoman decimaltoroman = null;
 
-		while (decimal <= 8) {
-			result += "I";
-			while (decimal < 4) {
-				result = "V" + result;
-			}
-			decimal--;
-		}
-		if (decimal == 5)
-			result = "V";
-
-		if (decimal == 4)
-			result = "IV";
-
-		while (decimal < 4) {
-			result += "I";
-			decimal--;
-		}
-		return result;
+	@Before
+	public void setUp() throws Exception {
+		decimaltoroman= new DecimalToRoman();
 	}
 
+	@Test
+	public void NullObjectTest() {
+		assertNotNull(decimaltoroman);
+	}
+
+	@Test
+	public void Decimal3(){
+		assertEquals("III", decimaltoroman.getRomanNumber(3));
+	}
+	
+	@Test
+	public void Decimal5(){
+		assertEquals("V", decimaltoroman.getRomanNumber(5));
+	}
+
+	@Test
+	public void Decimal4(){
+		assertEquals("IV", decimaltoroman.getRomanNumber(4));
+	}
+	
+	@Test
+	public void Decimal6(){
+		assertEquals("VI", decimaltoroman.getRomanNumber(6));
+	}
+	
+	@Test
+	public void Decimal9(){
+		assertEquals("IX", decimaltoroman.getRomanNumber(9));
+	}
+	
+	@Test
+	public void Decimal10(){
+		assertEquals("X", decimaltoroman.getRomanNumber(10));
+	}
+	
+	
 }
